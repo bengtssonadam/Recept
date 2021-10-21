bc6ec0467cfd6ebab4e1abafe4dd9fdd41298882
Adam Bengtsson <adambengtsson@MacBook-Pro-som-tillhor-Adam.local>
2021-10-21 14:27:38 +0200

    Created 2do tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        // lägga in maträtter
+        // lägga till ingridenser till maträtter
+        // lägga till recept för 4 personer
+        // lägga till recept för inmatat antal personer
+        // lägg till calorior för varje portion
+        // Spara recept till txt- fil
+        
     }
 }
